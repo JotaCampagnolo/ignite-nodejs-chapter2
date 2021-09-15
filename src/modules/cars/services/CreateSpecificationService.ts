e5b1983e74c9b34faa2e76673191232e180cfcbe
João M. Campagnolo <jota.campagnolo@gmail.com>
2021-09-15 17:11:48 -0300

    Specifications.

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("ToDo");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("This specification already exists.");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { request, response } from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.get("/", (request, response) => {
 });
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("🟢 Server is running!"));
