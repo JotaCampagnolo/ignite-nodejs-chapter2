0b65037e6cfb110191c7d1f24348a9b6e6d71e14
João M. Campagnolo <jota.campagnolo@gmail.com>
2021-09-15 17:13:21 -0300

    Importações desnecessárias removidas.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { request, response } from "express";
+import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
