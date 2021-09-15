fe21989b0d3fe99f6554989b05978361e3db1d7e
João M. Campagnolo <jota.campagnolo@gmail.com>
2021-09-15 20:24:47 -0300

    UseCases e Singleton Pattern.

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,4 +1,3 @@
-import { categoriesRoutes } from "../../../routes/categories.routes";
 import { Category } from "../models/Category";
 import {
   ICategoriesRepository,
@@ -8,8 +7,17 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
