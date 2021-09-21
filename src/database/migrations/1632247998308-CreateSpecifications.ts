8612da619d3c156d479bd5f1d46a22e03eb0b4b7
João M. Campagnolo <jota.campagnolo@gmail.com>
2021-09-21 15:30:53 -0300

    Refatorando as specifications.

---
+++ src/database/migrations/1632247998308-CreateSpecifications.ts
@@ -0,0 +1,41 @@
+import { query } from "express";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecifications1632247998308 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specifications");
+  }
+}
