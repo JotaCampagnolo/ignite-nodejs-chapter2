bf17aace49ccc2982a6aab9a57185607a61fcb19
João M. Campagnolo <jota.campagnolo@gmail.com>
2021-09-30 10:07:37 -0300

    Tratamento de exceções.

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email and password do not match!");
+      throw new AppError("Email and password do not match!");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Email and password do not match!");
+      throw new AppError("Email and password do not match!");
     }
 
     const token = sign({}, "c16cba8a0692f9e33cf4d8eba98a1e73", {
